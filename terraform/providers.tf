307fac2c99e29a61452b84a578092e9ae08e5d31
Filipp Fedorov <fedorov.f@selectel.ru>
2023-04-26 11:25:04 +0300

    Change get auth_url from var

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -6,7 +6,7 @@ provider "selectel" {
 # Инициализация провайдера OpenStack
 provider "openstack" {
   domain_name = local.domain_id
-  auth_url    = "https://api.selvpc.ru/identity/v3"
+  auth_url    = var.auth
   tenant_id   = module.project.project_id
   user_name   = module.project.user_name
   password    = module.project.user_password
